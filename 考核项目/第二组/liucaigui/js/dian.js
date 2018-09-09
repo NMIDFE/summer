5d22cf7e9a8e22aa87715a5b64e74caf05af1cd6
lcg2016 <33574272+lcg2016@users.noreply.github.com>
2018-09-09 18:34:59 +0800

    mod

--- 考核项目/第二组/liucaigui/js/dian.js
+++ 考核项目/第二组/liucaigui/js/dian.js
@@ -1,8 +1,11 @@
-var aas=document.getElementsByClassName('nav')[0].getElementsByTagName('a');
+var aas=document.getElementsByClassName('nav')[0].getElementsByClassName('left')[0].getElementsByTagName('a');
 var nav_2=document.getElementsByClassName('nav-2')[0];
 var lun=document.getElementsByClassName('lun')[0];
 var conn=document.getElementsByClassName('conn')[0];
 var page2=document.getElementsByClassName('page2')[0];
+var tri=document.getElementsByClassName('tri')[0];
+
+var tri2=document.getElementsByClassName('tri2')[0];
 aas[2].onclick=function(){
     nav_2.style.display="none";
     lun.style.display="none";
@@ -10,6 +13,8 @@ aas[2].onclick=function(){
     page2.style.display="block";
     aas[1].className="";
     aas[2].className="li_onbluer";
+    tri2.style.display="block";
+    tri.style.display="none";
 }
 aas[1].onclick=function(){
     page2.style.display="none";
@@ -18,4 +23,6 @@ aas[1].onclick=function(){
     conn.style.display="block";
     aas[2].className="";
     aas[1].className="li_onbluer";
+    tri2.style.display="none";
+    tri.style.display="block";
 }
